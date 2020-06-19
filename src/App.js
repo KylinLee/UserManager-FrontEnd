ab717ce730435e0848e48f5acc52451c623e3aae
KylinLee <kylincottage@163.com>
2020-06-19 22:58:57 +0800

    init project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+import { PrimaryButton } from '@fluentui/react';
+import { ApolloProvider } from '@apollo/react-hooks';
 import './App.css';
+import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
+
+const client = new ApolloClient({
+    uri: 'https://48p1r2roz4.sse.codesandbox.io',
+});
+
+client.query({
+    query: gql`
+    query {
+  rates(currency: "USD") {
+    rate
+    name
+    currency
+  }
+}
+  `
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    return (
+        <div className="App">
+            <header className="App-header">
+                <PrimaryButton>Hello</PrimaryButton>
+                <p>The Fluent Design Already Loaded</p>
+                <ApolloProvider client={client}>
+                    <div>
+                        <h2>My first Apollo app 🚀</h2>
+                    </div>
+                </ApolloProvider>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
         </a>
-      </header>
-    </div>
-  );
+            </header>
+        </div>
+    );
 }
 
 export default App;
