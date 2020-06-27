1354e3226436c03b7427df667b9cf6ec25e5ddff
KylinLee <kylincottage@163.com>
2020-06-27 23:05:34 +0800

    day-end

--- src/App.js
+++ src/App.js
@@ -1,61 +1,32 @@
 import React from "react";
-import { PrimaryButton } from "@fluentui/react";
-import { ApolloProvider } from "@apollo/react-hooks";
+import {PrimaryButton} from "@fluentui/react";
+import {ApolloProvider} from "@apollo/react-hooks";
 import "./App.css";
 import ApolloClient from "apollo-boost";
-import { gql } from "apollo-boost";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {gql} from "apollo-boost";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 import Home from "./pages/Home/Home.tsx";
 import About from "./pages/About/About.tsx"
 
-const client = new ApolloClient({
-    uri: "https://48p1r2roz4.sse.codesandbox.io",
-});
-
-client.query({
-    query: gql`
-        query {
-            rates(currency: "USD") {
-                rate
-                name
-                currency
-            }
-        }
-    `,
-});
-
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <PrimaryButton>Hello</PrimaryButton>
-                <p>The Fluent Design Already Loaded</p>
-                <ApolloProvider client={client}>
-                    <div>
-                        <h2>My first Apollo app <span role="img" aria-label="rocket">🚀</span></h2>
-                    </div>
-                </ApolloProvider>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-
             <Router>
-                <Link to="/home">Home</Link>
-                <Link to={"/about"}>About</Link>
+                <Link to="/">Root</Link> |
+                <Link to={"/home"}>HOME</Link> |
+                <Link to={"/about"}>ABOUT</Link>
                 <Switch>
-                    <Route path="/home">
+                    <Route path={"/home"}>
                         <Home/>
                     </Route>
-                    <Route path="/about">
+                    <Route path={"/about"}>
                         <About/>
                     </Route>
+                    <Route path={"/"}>
+                        <Home/>
+                    </Route>
+
                 </Switch>
             </Router>
         </div>
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./About.scss"
 
 function About() {
     return <h1>About Page</h1>;
