245c1f61aebece97b09efcd28a99ae67d706d208
KylinLee <kylincottage@163.com>
2020-07-17 17:33:37 +0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import React from "react";
-import { PrimaryButton } from "@fluentui/react";
 import { ApolloProvider } from "@apollo/react-hooks";
 import "./App.css";
 import ApolloClient from "apollo-boost";
 import { gql } from "apollo-boost";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Home from "./pages/Home/Home.tsx";
-import About from "./pages/About/About.tsx"
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 function App() {
     return (
         <div className="App">
             <Router>
-                <Link to="/">Root</Link> |
-                <Link to={"/home"}>HOME</Link> |
+                <section></section>
+                <Link to="/">Root</Link> |<Link to={"/home"}>HOME</Link> |
                 <Link to={"/about"}>ABOUT</Link>
                 <Switch>
                     <Route path={"/home"}>
